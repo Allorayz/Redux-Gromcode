79672ce070a7bd98be189358f6b2f5a4cda56bc7
Oleksii Ulianov <alexallorayz@gmail.com>
2024-08-26 16:51:34 +0300

    Redux actions with data in payload

--- lesson2-Intro-to-Redux/task3-redux-actions-payloads/src/index.js
+++ lesson2-Intro-to-Redux/task3-redux-actions-payloads/src/index.js
@@ -2,9 +2,11 @@ import "./index.scss";
 import store from "./store";
 import { addUser, deleteUser } from "./users.actions";
 
-store.dispatch(addUser({ id: 76, name: "Alex" }));
-store.dispatch(addUser({ id: 726, name: "Bob" }));
-store.dispatch(addUser({ id: 7261, name: "Krab" }));
-store.dispatch(addUser({ id: 7262, name: "Krabasdasdas" }));
-store.dispatch(deleteUser(7262));
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(addUser({ id: 16, name: "Alex" }));
+store.dispatch(addUser({ id: 13126, name: "Nino" }));
+store.dispatch(addUser({ id: 1132312316, name: "Bred" }));
+store.dispatch(deleteUser(1132312316));

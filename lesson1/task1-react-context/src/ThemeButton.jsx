1927172447255b87e7f3a5ccde36052c37c96da0
Oleksii Ulianov <alexallorayz@gmail.com>
2024-08-26 12:41:50 +0300

    React-context

---
+++ lesson1/task1-react-context/src/ThemeButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ThemeContext } from "./themes-context";
+import { themes } from "./themes-context";
+
+class ThemeButton extends React.Component {
+  render() {
+    console.log(this.context);
+    return (
+      <button
+        {...this.props}
+        style={{
+          backgroundColor: this.context.background,
+          color: this.context.fontColor,
+        }}
+        className="btn"
+      />
+    );
+  }
+}
+
+ThemeButton.contextType = ThemeContext;
+
+export default ThemeButton;
